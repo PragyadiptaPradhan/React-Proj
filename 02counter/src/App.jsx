c2fe5af6f930cca93294665f08b8fc142d065191
Hrisi Pradhan <pragyadipta84@gmail.com>
2025-06-11 16:38:49 +0530

    update lec 02

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,11 +5,22 @@ import './App.css'
 
 function App() {
 
-  let counter = 15
+  let [counter, setCounter] = useState(15)
+
+  //let counter = 15
 
   const addValue = () => {
-    console.log("clicked", counter);
-    counter = counter + 1;
+    //counter = counter + 1;
+    if (counter < 20 ){
+      setCounter(counter + 1)
+    }
+
+  }
+
+  const removeValue = () => {
+    if ( counter > 0){
+      setCounter(counter - 1)
+    }
   }
 
   return (
@@ -21,7 +32,9 @@ function App() {
         onClick={addValue}
       >Add value {counter}</button>
       <br />
-      <button>Remove value {counter}</button>
+      <button
+      onClick={removeValue}
+      >Remove value {counter}</button>
       <p>footer: {counter}</p>
     </>
   )
